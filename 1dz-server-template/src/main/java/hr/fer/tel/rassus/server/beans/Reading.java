f0c010613087932166f8dfa2d3c6915b432c8b56
Josip Rubčić <josiprubcic2004@gmail.com>
2025-11-01 17:05:56 +0100

    updated dependencies, sensor part modified in server directory

--- 1dz-server-template/src/main/java/hr/fer/tel/rassus/server/beans/Reading.java
+++ 1dz-server-template/src/main/java/hr/fer/tel/rassus/server/beans/Reading.java
@@ -2,6 +2,57 @@ package hr.fer.tel.rassus.server.beans;
 
 public class Reading {
   //  TODO
+    private double temperature, humidity, pressure, co, no2, so2;
 
+    public Reading() {
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getCo() {
+        return co;
+    }
+
+    public void setCo(double co) {
+        this.co = co;
+    }
+
+    public double getNo2() {
+        return no2;
+    }
+
+    public void setNo2(double no2) {
+        this.no2 = no2;
+    }
+
+    public double getSo2() {
+        return so2;
+    }
+
+    public void setSo2(double so2) {
+        this.so2 = so2;
+    }
 }
 
--- 1dz-server-template/src/main/java/hr/fer/tel/rassus/server/beans/Sensor.java
+++ 1dz-server-template/src/main/java/hr/fer/tel/rassus/server/beans/Sensor.java
@@ -1,5 +1,44 @@
 package hr.fer.tel.rassus.server.beans;
 
 public class Sensor {
-  //  TODO
+  private final long id;
+  private final double temperature, pressure, humidity, co, no2, so2;
+
+    public Sensor(long id, double temperature, double pressure, double humidity, double co, double no2, double so2) {
+        this.id = id;
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.co = co;
+        this.no2 = no2;
+        this.so2 = so2;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public double getCo() {
+        return co;
+    }
+
+    public double getNo2() {
+        return no2;
+    }
+
+    public double getSo2() {
+        return so2;
+    }
 }
